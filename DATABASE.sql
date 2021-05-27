b89c283640e052b6001ae88e9e5667b60b9f63bb
James Kelley <jkkelley86@gmail.com>
2021-05-27 13:29:37 -0500

    added database

---
+++ DATABASE.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "koalas" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80),
+	"gender" VARCHAR (1),
+	"age" INT,
+	"ready-to-transfer" boolean,
+	"notes" VARCHAR(100)
+
+);
+
+INSERT INTO "koalas"("name", "gender", "age", "ready-to-transfer", "notes") 
+VALUES 
+('Scotty', 'M', '4', 'true', 'Born in Guatemala'),
+('Jean', 'F', '5', 'true', 'Allergic to lots of lava'),
+('Ororo', 'F', '7', 'false', 'Loves listening to Paula (Abdul)'),
+('Logan', 'M', '15', 'false', 'Loves the sauna'),
+('Charlie', 'M', '9', 'true', 'Favorite band is Nirvana'),
+('Betsy', 'F', '4', 'true', 'Has a pet iguana');
